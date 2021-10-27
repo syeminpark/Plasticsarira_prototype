5f986d1e5a1a112cfa4788802d02b5f1cbcf62ef
macbookPro <soccer0874@naver.com>
2021-10-27 23:25:20 +0900

    towindows

--- src/Sarira.js
+++ src/Sarira.js
@@ -2,16 +2,12 @@ class Sarira {
     constructor(bufferGeometry) {
         this.plasticList = []
         this.plasticList.push(new Core())
-        this.plasticList[0].initialize(bufferGeometry)
-        
+        this.plasticList[0].initialize()
+        this.plasticList[0].updateBuffer(bufferGeometry,this.plasticList.length)
     }
 
     addPlastics(micro) {
         this.plasticList.push(micro)
     }
-    showPlastics() {
-        for (let plastic of this.plasticList) {
-            plastic.show();
-        }
-    }
+
 }
