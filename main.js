3bb2e98b46332ec23d2215a68396e5257327eb74
SeungheeChoi <udongSH900@gmail.com>
2021-10-28 20:17:37 +0900

    add microPlastics_world

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let threeSystemController
 let bodySystem;
 
+let microPlastics;
+
 setup()
 draw()
 
@@ -9,8 +11,10 @@ function setup() {
     threeSystemController = new ThreeSystemController();
     bodySystem= new BodySystem();
 
+    microPlastics = new MicroPlastic_Particles(3000, 50);
+
     ///예시 코드 
-    threeSystemController.addToWorldScene(createCube())
+    threeSystemController.addToWorldScene(microPlastics.points);
 }
 
 function draw() {
@@ -18,6 +22,8 @@ function draw() {
     //매 프레임 실행되는 부분
     threeSystemController.update()
     bodySystem.update()
+
+    microPlastics.update();
 }
 
 //------------------------------------------------------------------
