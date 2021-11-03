49d9f14a2db6f503e6deaa2d75d1638194c15930
macbookPro <soccer0874@naver.com>
2021-11-03 21:15:54 +0900

    fixtoUseNormalSlider

--- src/TerminalController.js
+++ src/TerminalController.js
@@ -6,9 +6,9 @@ class TerminalController {
         this.categoryTextSize = "0.7vw"
         this.metaDataTextSize = "0.5vw"
         this.wordSpacingList = [0, 2.3, 4, 6.5, 3.5, 6]
-
-        this.initialSpace = 35
         this.space = 2
+        this.initialSpace = pxToVh(document.querySelector('#sarira').getBoundingClientRect().bottom) + this.space
+
         this.ownerVerticalSpace = 0
         this.leftPosition = 71
 
