ebb1f4a78e7a36b3dff7f8ee511a1caca586ed4f
syeminPark <soccer0874@gmail.com>
2021-11-03 13:20:31 +0900

    +

--- src/Slider.js
+++ src/Slider.js
@@ -1,24 +1,32 @@
 class Slider {
 
     constructor() {
-        this.slider = document.querySelector('#slider')
-        this.sliderRect = slider.getBoundingClientRect()
 
+        this.slider = document.getElementById('mySlider')
+        this.sliderRect = this.slider.getBoundingClientRect()
+
+        this.sliderContainer =  this.sliderRect
+        
         window.addEventListener('resize', this.updateSliderRect.bind(this));
     }
 
     isWithin(event) {
         if (event.clientX > this.sliderRect.left && event.clientY > this.sliderRect.top && event.clientY < this.sliderRect.bottom) {
+
             return true
         }
         return false
     }
 
-    moveSlider() {
-        this.sliderRect
+    moveSlider(event) {
+
+        let dist= event.clientY - this.sliderContainer.top
+        this.slider.style.top =dist+"px"
+
     }
 
     updateSliderRect() {
-        this.sliderRect = slider.getBoundingClientRect()
+
+        this.sliderRect = this.slider.getBoundingClientRect()
     }
 }
