6228612c43ef44bbdbd90846afacc1391265c90a
macbookPro <soccer0874@naver.com>
2021-11-03 15:31:32 +0900

    fixsize

--- src/Slider.js
+++ src/Slider.js
@@ -3,19 +3,24 @@ class Slider {
     constructor() {
         this.slider = document.querySelector('#slider')
         this.sliderRect = slider.getBoundingClientRect()
+      
 
         window.addEventListener('resize', this.updateSliderRect.bind(this));
     }
 
     isWithin(event) {
         if (event.clientX > this.sliderRect.left && event.clientY > this.sliderRect.top && event.clientY < this.sliderRect.bottom) {
+            this.moveSlider(event.clientY)
             return true
+          
         }
         return false
     }
 
-    moveSlider() {
-        this.sliderRect
+    moveSlider(mouseYPosition) {
+       print("true")
+       print(this.slider.style.top)
+        constrain(this.sliderRect.top)
     }
 
     updateSliderRect() {
