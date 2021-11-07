184c9ecf7e79d9358e08633de19daa4a42df0921
syeminPark <soccer0874@gmail.com>
2021-11-08 04:35:11 +0900

    +

--- src/Microplastic.js
+++ src/Microplastic.js
@@ -81,8 +81,8 @@ class Microplastic {
         for (let i = 0; i < others.length; i++) {
 
             let d2 = this.positionVector3.distanceTo(others[i].positionVector3)
-            if ((d2 < this.size + others[i].size)) {
-                print("true")
+            if ((d2 < this.size + others[i].size) +
+                (this.tensileStrength + others[i].tensileStrength) / 2) {
                 return true
             }
         }
@@ -108,8 +108,6 @@ class Microplastic {
         return today.getHours() >= 12 ? 'PM' : 'AM';
     }
 }
-
-
 //move 적용시키기. positionVecor3 this.positionList 과정 복습하기. 충돌 안일어나게 하기. move적용시 다르게 생기는 것 방지하기 
 //더 작게 만들기 
-// convex제대로 만들어지는지 확인하기 
+// convex제대로 만들어지는지/
