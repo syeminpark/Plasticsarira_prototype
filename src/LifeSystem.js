aa36118dda2aa93a46360f836e9c2f226d20d1e2
SeungheeChoi <udongSH900@gmail.com>
2021-11-08 07:18:11 +0900

    생물 분류

--- src/LifeSystem.js
+++ src/LifeSystem.js
@@ -1,6 +1,10 @@
 class LifeSystem{
     constructor(threeSystemController){
-        this.num = 15;
+        this.primaryNum = 10;
+        this.secondaryNum = 6;
+        this.tertiaryNum = 3;
+        this.num = 1 + this.primaryNum + this.secondaryNum + this.tertiaryNum;
+
         this.windowSize = 140;
 
         this.lifes = [];
@@ -9,8 +13,17 @@ class LifeSystem{
         this.lifes.push(this.life_user);
 
         for (let i = 1; i < this.num; i++) {
-            this.l = new Life(i, this.windowSize,threeSystemController);
-            this.lifes.push(this.l);
+            if (i < 1+this.primaryNum) {
+                const l = new Life_primaryConsumer(i, this.windowSize,threeSystemController);
+                this.lifes.push(l);
+            } else if (i < 1+this.primaryNum+this.secondaryNum && i >= 1+this.primaryNum){
+                const l = new Life_secondaryConsumer(i, this.windowSize,threeSystemController);
+                this.lifes.push(l);
+            } else {
+                const l = new Life_tertiaryConsumer(i, this.windowSize,threeSystemController);
+                this.lifes.push(l);
+            }
+            
         }
 
         this.display();     
