bf787f1b47f1a4533a10be8352e7b7482d119d29
syeminPark <soccer0874@gmail.com>
2021-12-14 21:54:35 +0900

    sync

--- src/ThreeSystemController.js
+++ src/ThreeSystemController.js
@@ -43,6 +43,7 @@ class ThreeSystemController {
         //orbit controls, renderer, frame rate 업데이트 
         this.sariraThreeSystem.update()
         this.worldThreeSystem.update()
+    
 
         //매 프레임 렌더링하기 
         this.render()
