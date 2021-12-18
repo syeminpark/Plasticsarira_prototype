0d5b798382604711521b43ff5e33bca9662be085
syeminPark <soccer0874@gmail.com>
2021-12-18 17:17:01 +0900

    tidy

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ let particleSystem_microPlastic
 let bodySystemController
 let serverClientCommunication
 let userDead = false;
-
-
-  
+let virtualKeyboard 
 
 checkDeviceType()
 moveToTopWindow()
@@ -15,43 +13,31 @@ done()
 
 
 async function setup() {
-
-
     let dataOrganizer= new DataOrganizer(document.getElementById('userName').textContent)
     serverClientCommunication = new ServerClientCommunication(dataOrganizer)
     serverClientCommunication.createUser()
-
-
-
     threeSystemController = new ThreeSystemController();
-    
-    let virtualKeyboard = new VirtualKeyboard()
-    console.log(virtualKeyboard)
+    virtualKeyboard = new VirtualKeyboard()
     lifeSystem = new LifeSystem(virtualKeyboard);
     particleSystem_microPlastic = new ParticleSystem(lifeSystem);
     particleSystem_microPlastic.display(threeSystemController, 0.3)
 
-        
     await userHoverGuide()
     await userSmallWindowGuide()
-
     reloadCss()
 }
 
 function draw() {
     requestAnimationFrame(draw);
     if (document.getElementById('userName')) {
-        //
         threeSystemController.update()
         particleSystem_microPlastic.update();
         lifeSystem.update();
-        
     }
 }
 
 async function done() {
     requestAnimationFrame(done);
-    
     if (userDead) {
         userDead = false;
         await serverClientCommunication.postSariraById(lifeSystem.life_user.getSariraDataForServer())
--- src/customFunctions.js
+++ src/customFunctions.js
@@ -40,7 +40,7 @@ function createDomElement(type, isDisplay, id, innerHtml, doc) {
     dom.style.display = "none"
   }
   dom.id = id
-  
+
   dom.innerHTML = innerHtml
   doc.appendChild(dom)
 }
@@ -49,26 +49,27 @@ function checkAccessRoute() {
   if (!sessionStorage.getItem("userId")) {
     window.location.href = 'https://greenverse.art/artworks/plastic-sarira/';
   } else {
-    createDomElement("div", false, "userId", sessionStorage.getItem("userId"),document.body)
-    createDomElement("div", false, "userName", sessionStorage.getItem("userName"),document.body)
+    createDomElement("div", false, "userId", sessionStorage.getItem("userId"), document.body)
+    createDomElement("div", false, "userName", sessionStorage.getItem("userName"), document.body)
   }
 }
 
-function reloadCss()
-{
-    let links = document.getElementsByTagName("link");
-    for (var cl in links)
-    {
-        var link = links[cl];
-        if (link.rel === "stylesheet")
-            link.href += "";
-    }
+function reloadCss() {
+  let links = document.getElementsByTagName("link");
+  for (var cl in links) {
+    var link = links[cl];
+    if (link.rel === "stylesheet")
+      link.href += "";
+  }
 }
 
-function moveToTopWindow(){
-let input =document.querySelector("input")
-  input.addEventListener("input", (event)=>{
-    window.scrollTo({top:0, behavior:'smooth'});
-    
-  },true)
+function moveToTopWindow() {
+  let input = document.querySelector("input")
+  input.addEventListener("input", (event) => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+
+  }, true)
 }
